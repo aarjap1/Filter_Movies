5cdb216e507dd7cd80fd0da236ceb2a8669e34d8
aarjap1 <piyaaarjap@gmail.com>
2024-05-11 12:54:03 -0500

    dashboard updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [username, setUsername] = useState("Guest");
   return (
-    <nav className="container">
-      <div className="title-1">MovieMania</div>
-      <div>
-        <ul>
-          <li href="#">My Favorites</li>
-          <li href="#">Watch List</li>
-          <li href="#">Genre</li>
-        </ul>
-      </div>
-      <div>
-        <input
-          className="search-bar"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-      </div>
-    </nav>
+    <>
+      <nav className="navigator">
+        <div className="title-1">MovieMania</div>
+        <div>
+          <ul>
+            <li href="#">My Favorites</li>
+            <li href="#">Watch List</li>
+            <li href="#">Genre</li>
+          </ul>
+        </div>
+        <div className="search">
+          <input
+            className="search-bar"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </div>
+      </nav>
+      <div className="welcome">Welcome {username}</div>
+    </>
   );
 };
 
