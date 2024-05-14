e2e3608125ca7dd465f8240d7c880fdeb00855ec
aarjap1 <piyaaarjap@gmail.com>
2024-05-13 22:02:20 -0500

    scroll bar removed

--- src/main.jsx
+++ src/main.jsx
@@ -29,6 +29,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <GoogleOAuthProvider clientId="1028426619274-mjhob9l103hsas4fjpcif0lsbjg337rq.apps.googleusercontent.com">
       <RouterProvider router={router} />
     </GoogleOAuthProvider>
-    ;
   </React.StrictMode>
 );
